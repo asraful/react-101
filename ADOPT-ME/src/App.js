409369322a765e454e3aca705c809665ea14fc94
Asraful forhad chowdhury <asraful@Asrafuls-MacBook-Pro.local>
2023-02-27 11:18:37 +0200

    one way data flow

--- ADOPT-ME/src/App.js
+++ ADOPT-ME/src/App.js
@@ -1,9 +1,9 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div",{},[
-        React.createElement("h1",{},"Luna"),
-        React.createElement("h2",{},"Dog"),
-        React.createElement("h3",{},"Havanese")
-    ])
+        React.createElement("h1",{},props.name),
+        React.createElement("h2",{},props.animal),
+        React.createElement("h3",{},props.breed),
+    ]);
 };
 
 
@@ -13,9 +13,22 @@ const App = () => {
     {},
     [
         React.createElement("h1",{},"Adopt me!"),
-        React.createElement(Pet),
-        React.createElement(Pet),
-        React.createElement(Pet)
+        React.createElement(Pet, {
+            animal: "Dog",
+            name: "Luna",
+            breed: "Havanese"
+
+        }),
+        React.createElement(Pet, {
+            animal: "Bird",
+            name: "Name",
+            breed: "Mixed"
+        }),
+        React.createElement(Pet, {
+            animal: "Cat",
+            name: "Whitecat",
+            breed: "mixed"
+        }),
     ]
     )
 };
