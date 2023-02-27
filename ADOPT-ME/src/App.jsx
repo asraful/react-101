73e9db77a514bc61392d97adb8f198c391047fc5
asraful <asraful.cse@gmail.com>
2023-02-27 13:46:47 +0200

    introduce prettier,ESLint,Vite

---
+++ ADOPT-ME/src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { createRoot } from "react-dom";
+
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Luna",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      animal: "Bird",
+      name: "Name",
+      breed: "Mixed",
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Whitecat",
+      breed: "mixed",
+    }),
+  ]);
+};
+
+const continer = document.getElementById("root");
+const root = createRoot(continer);
+root.render(React.createElement(App));
