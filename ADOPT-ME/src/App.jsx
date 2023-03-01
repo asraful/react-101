c311ffca87fa542d5a7aeb1221dadda4f39e63e4
asraful <asraful.cse@gmail.com>
2023-03-01 11:06:33 +0200

    react router, browser rounter

--- ADOPT-ME/src/App.jsx
+++ ADOPT-ME/src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt me!</h1>
-      <SearchParams />
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
